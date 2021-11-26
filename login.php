57d6bfc8ea44ca85bc47701fb7faf91e7252193f
VarzikaT <varzikatiwari08@gmail.com>
2021-11-27 02:37:05 +0530

    database updation

--- login.php
+++ login.php
@@ -14,11 +14,19 @@ if(mysqli_num_rows($data)>0)
 	{
 		if($row['UserID']==$num && $row['Password']==$pass)
 		{
-			echo "home page";
-			break;
+			 echo "Succesfully logged-in";
+			 header("refresh:1; url=index.html");
+			 break;
+			// echo "<script>alert('successfully logged-in')</script>";
+			// echo "<script>location.href='index.html'</script>";
 		}
-		
+	// 	else{
+	// 	echo "<script>alert('incorrect id or password')</script>";
+	//  echo "<script>location.href='login.html'</script>";
+	// 	// echo "Incorrect Password";
+	// 	// header("refresh:1; url=login.html");
+	 }
+		}
 	}
-}
 }
 ?>
